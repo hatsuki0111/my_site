93f7c2b76af2d9cbecc8559e4620d82007b7eba9
hatsuki0111 <cist.b217.h.saitou@gmail.com>
2020-07-28 00:01:59 +0900

    first commit

---
+++ src/constants/contentful.js
@@ -0,0 +1,4 @@
+export default {
+  space: '2qgj2z5v5bl2',
+  accessToken: '84BoNFkpWbELcaaxTtN8MBdHoBi7dzs6cvl4ICaxCqk'
+}
---
+++ src/pages/Top.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import BlogCard from '../components/BlogCard'
+
+const Top =(props)=>{
+  return(
+    <>
+    <div className='top'>Top</div>
+    <div onClick={()=>console.log(props.data)}>clg</div>
+    <p>{props.data.length ? props.data.map((item,i)=>(
+      <BlogCard data={item} key={i} />
+        )):
+        (<p>Now Loading...</p>)}
+    </p>
+    </>
+    )
+    }
+
+export default Top
